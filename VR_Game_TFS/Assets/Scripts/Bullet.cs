8e5e05b1eb9bfe9b9e24d32b74bdee967147b022
luke_fent <lfentress@gmail.com>
2019-08-14 21:50:18 -0400

    Ragdoll set-up
    Ragdolls set up. Interactable class set up. Bullet set up. Cheap 1st person controller set up.

---
+++ VR_Game_TFS/Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float thrust; 
+    private Rigidbody body;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    public void Fired()
+    {
+        body = GetComponent<Rigidbody>();
+        body.AddForce(transform.forward * thrust, ForceMode.Impulse);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.GetComponent<InteractClass>())
+        {
+            other.transform.GetComponent<InteractClass>().Interact();
+        }
+    }
+}
---
+++ VR_Game_TFS/Assets/Scripts/RagDollController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagDollController : MonoBehaviour
+{
+
+    public Animator ani; 
+    public Rigidbody[] ragDoll = new Rigidbody[13];
+    public BoxCollider box; 
+    
+
+    public bool TurnOn = true;
+
+    public void InteractWithMe()
+    {
+        TurnOnRagDoll(false);
+    }
+
+    public void TurnOnRagDoll(bool value)
+    {
+        for (int i = 0; i < ragDoll.Length; i++)
+        {
+            ragDoll[i].isKinematic = value;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TurnOnRagDoll(true);
+    }
+
+    public void KnockAbout()
+    {
+        box.enabled = false;
+        ani.enabled = false;
+        TurnOnRagDoll(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(TurnOn == false)
+        {
+            ani.enabled = false; 
+            TurnOnRagDoll(false);
+        }
+
+    }
+}
+
